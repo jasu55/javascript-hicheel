0ee6510b0277585823e4cf0e7fa849e5802e827d
jamka <67340668+jasu55@users.noreply.github.com>
2025-07-23 01:22:51 +0800

    7.22 gert

--- function.js
+++ function.js
@@ -38,10 +38,25 @@ function findMin(a, b, c) {
 }
 findMin(40, 50, 6);
 
-// bodlogo1 
-function print1toNnumber
+// bodlogo1 1s n toonuudiig hewle
+function print1toNnumbers(n) {
+  for (i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+print1toNnumbers(6);
 
-// bodlogo3
+// bodlogo2 1s N too hvrtel tegsh toonuudiig hewle
+function print1toNevenNumbers(n) {
+  for (i = 1; i <= n; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+print1toNevenNumbers(6);
+
+// bodlogo3 1s N too hvrtelh niilber
 
 function findSum(n) {
   let sum = 0;
@@ -52,28 +67,91 @@ function findSum(n) {
 }
 findSum(6);
 
-//bodlogo6
-function findCountDigits(n){
-  let i=0  
-  for(a=n;a>1;a/=10){
-       i++
-    }console.log(i)
-}
-findCountDigits(1234)
-
-let number=123
-let i=0
-while(number>1){i=i+1
-    number/=10
-    
-}console.log(i)
-let size=5
-for(i=0;i<size;i++){
-  let star=""
-  for(j=0;j<size-i-1;j++){
-    star+="-"
+// bodlogo4 1s N too hvrtelh sondgoi toog ol
+function print1toNoddNumbers(n) {
+  for (i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      console.log(i);
+    }
   }
-  for(j=0;j<=4;j++){
-    star+="*"
-  }console.log(star)
 }
+print1toNoddNumbers(6);
+
+//bodlogo 5 n toonii vrjweriin hvrd hewle
+function printMultiplicationTable(n) {
+  for (i = 1; i < 10; i++) {
+    console.log(n * i);
+  }
+}
+printMultiplicationTable(6);
+
+//bodlogo6
+function findCountDigits(n) {
+  let a = 0;
+  for (i = n; i > 1; i /= 10) {
+    a++;
+  }
+  console.log(a);
+}
+findCountDigits(1234);
+
+// bodlogo 7 ehnii bolon svvliin orong ol
+function findFirstAndLastDigit(n) {
+  // ehnii oron
+  for (i = n; i >= 10; i /= 10) {}
+  console.log(parseInt(i));
+  //svvliin oron
+  console.log(n % 10);
+}
+findFirstAndLastDigit(4332);
+
+//bodlogo8 ehnii ba svvliin orongiin niilberiig ol
+function findSumOfFirstAndLastDigits(n) {
+  for (i = n; i >= 10; i /= 10) {}
+  console.log(parseInt(i) + (n % 10));
+}
+findSumOfFirstAndLastDigits(4312311);
+
+//bodlogo9 ehnii bolon svvlin orong solj bich
+function changeFirstAndLastDigits(n) {
+  let c = n;
+  for (j = 0; c >= 1; j++) {
+    c /= 10;
+  }
+  let countDigits = j;
+  for (i = n; i >= 10; i /= 10) {}
+  let a = parseInt(i) % 10;
+  let b = n % 10;
+  n = n - (a - b) * 10 ** (countDigits - 1) + (a - b);
+
+  console.log(n);
+}
+changeFirstAndLastDigits(1234567);
+
+//bodlogo 10 tsifrvvdiin niilberiig ol
+function findSumOfDigits(n) {
+  let s = 0;
+  for (i = n; i >= 1; i /= 10) {
+    let a = parseInt(i % 10);
+    s += a;
+  }
+  console.log(s);
+}
+findSumOfDigits(5464);
+
+//bodlogo11 too urwuu daraallaar hewle
+function reverseDigitsDigits(n) {
+  let c = n;
+  for (j = 0; c >= 1; j++) {
+    c /= 10;
+  }
+  let countDigits = j;
+  for (d = 1; d <= countDigits / 2; d++) {
+    for (i = n; i >= 10 ** d; i /= 10) {}
+    let a = parseInt(i) % 10;
+    let b = parseInt((n / 10 ** (d - 1)) % 10);
+    n = n - (a - b) * 10 ** (countDigits - d) + (a - b) * 10 ** (d - 1);
+  }
+  console.log(n);
+}
+reverseDigitsDigits(1234567);
